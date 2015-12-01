431552e4c0b909489be173a8134e357cf63c4fca
DavidScherArazi <ds13ae@my.fsu.edu>
2015-12-01 12:19:32 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,55 @@
+<HTML lang="en">
+<head> 
+<title> Events List </title>
+<!-- Tabs at the top of the page -->
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+li {
+    float: left;
+}
+
+a:link, a:visited {
+    display: block;
+    width: 120px;
+    font-weight: bold;
+    color: #FFFFFF;
+    background-color: #98bf21;
+    text-align: center;
+    padding: 4px;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+a:hover, a:active {
+    background-color: #7A991A;
+}
+</style>
+<div id="ts_tabmenu">
+    <ul>
+        <li><a href=index.php><strong>Index</strong></a></li>
+        <li><a href=profile.php><strong>My Profile</strong></a></li>
+        <li><a href=eventlist.php><strong>Event List</strong></a></li>
+        <li><a href=orglist.php><strong>Org List</strong></a></li>
+        <li><a href=about.php><strong>About</strong></a></li>
+    </ul>
+</div>	
+</head>
+
+<body>
+
+<font size = "24"> <center> Tallahassee events is a student project for COP4710. </center> </font> 
+<font size = "22"> <center> By David Scher-Arazi </center> </font>
+FSUID: ds13ae
+<br>Date: December 1st, 2015
+<br>Instructor: Peixiang Zhao
+<br>Section: 3 
+
+
+</body>
+</html>
